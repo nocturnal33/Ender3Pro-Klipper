d258a85169c8477aadf0eda879e84425417d5849
nocturnal33 <nocturnal33@users.noreply.github.com>
2024-10-30 20:37:47 -0400

    Update start_leds.sh

--- start_leds.sh
+++ start_leds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+## Written by Nathan (nocturnal33)
+## 2024.10.10
 source /home/pi/adafruit_env/bin/activate
-sudo -E env "PATH=$PATH" python3 /home/pi/scripts/klipper_neopixel2.py
+sudo -E env "PATH=$PATH" python3 /home/pi/scripts/klipper_led.py
